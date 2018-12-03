128c05df74c0531fd90affecd5e551621bf3137e
Darong Mean <darong012@gmail.com>
2018-12-03 22:42:15 +0700

    solution to day 01 part 01

--- test/adventofcode/core_test.clj
+++ test/adventofcode/core_test.clj
@@ -1,3 +1,10 @@
 (ns adventofcode.core-test
-  (:require [clojure.test :refer :all]
-            [adventofcode.core :refer :all]))
+  (:require
+    [clojure.test :refer :all]
+    [adventofcode.core]
+    [adventofcode.day01 :as day01]))
+
+
+(deftest day01-test
+  (testing "part 01"
+    (is (= 595 (day01/frequency)))))
